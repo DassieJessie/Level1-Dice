bea0a2ca04e787b0baa6b7842e12b66b9fe60810
Jienie12 <jr.egmond@gmail.com>
2020-03-17 17:21:23 +0100

    Swipe + Check answer
    All done <3

--- Level 2/Task 2/app/src/main/java/com/example/swipequiz/MainActivity.kt
+++ Level 2/Task 2/app/src/main/java/com/example/swipequiz/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.swipequiz
 
+import android.content.ClipData
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -30,8 +33,49 @@ class MainActivity : AppCompatActivity() {
         questions.add(Questions(getString(R.string.unit), true))
         questions.add(Questions(getString(R.string.whenSwitch), true))
 
-        //val itemTouchHelperCallback = object:
+        touchSwipe().attachToRecyclerView(idRecyclerView)
+
     }
 
-    //private fun
+    private fun touchSwipe(): ItemTouchHelper {
+        val itemTouchHelperCallback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                //false because dragDirs = 0
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                when (direction) {
+                    ItemTouchHelper.RIGHT ->
+                        checkAnswer(position, true)
+
+                    ItemTouchHelper.LEFT ->
+                        checkAnswer(position, false)
+                }
+                //zonder crashed de app wanneer je een item wilt deleten
+                viewAdapter.notifyDataSetChanged()
+
+            }
+
+        }
+
+        return ItemTouchHelper(itemTouchHelperCallback)
+    }
+
+    private fun checkAnswer(position: Int, right: Boolean){
+
+        if(questions[position].questionBool == right) {
+            questions.removeAt(position)
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
 }
