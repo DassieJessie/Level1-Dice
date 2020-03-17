12eab2660d80a935dee348856dcd33e817104d22
Jienie12 <jr.egmond@gmail.com>
2020-03-17 13:16:39 +0100

    XML + Button test

--- Level 1/Task 2/app/src/main/java/com/example/logicaapp/LogicaAppActivity.kt
+++ Level 1/Task 2/app/src/main/java/com/example/logicaapp/LogicaAppActivity.kt
@@ -2,11 +2,22 @@ package com.example.logicaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_logica_app.*
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logica_app)
+
+        //click on confirm
+        btnConfirm.setOnClickListener{
+            onClickConfirm()
+        }
+    }
+
+    private fun onClickConfirm() {
+        Toast.makeText(this,  getString(R.string.test), Toast.LENGTH_LONG).show()
     }
 }
